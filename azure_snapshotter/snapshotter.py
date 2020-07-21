b84111cef9c278c4ce323fd431f128bca0eea3a8
Maik Wild <mail@maikwild.de>
2020-07-21 19:27:44 +0200

    implement diff based upload

--- azure_snapshotter/snapshotter.py
+++ azure_snapshotter/snapshotter.py
@@ -61,6 +61,10 @@ def _put(key, value):
     ciphertext = encrypt(value)
     _store.put(_prefix + key, ciphertext)
 
+def _put_data(key, value):
+    ciphertext = encrypt(value)
+    _store.put(_partition + "/files/" + key, ciphertext)
+
 
 def _get(key):
     try:
@@ -69,39 +73,65 @@ def _get(key):
         return None
     return decrypt(ciphertext)
 
+def _get_data(key):
+    try:
+        ciphertext = _store.get(_partition + "/files/" + + key)
+    except KeyError:
+        return None
+    return decrypt(ciphertext)
+
 
 def _keys():
     return [x[len(_prefix) :] for x in _store.keys(prefix=_prefix)]
 
+def _files():
+    return [x[len(_partition + "/files/") :] for x in _store.keys(prefix=_partition + "/files/")]
 
-def _backup_dir(path):
+
+def md5(file_name):
+    hash_md5 = hashlib.md5()
+    with open(file_name, "rb") as f:
+        for chunk in iter(lambda: f.read(16384), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+def _backup_dir(path, files = None):
+    files = files if files else _files()
     for f in os.listdir(path):
         full_file_path = os.path.join(path, f)
 
         if os.path.isdir(full_file_path):
-            backup_dir(full_file_path)
+            _backup_dir(full_file_path, files)
         else:
-            print(full_file_path)
-            key = full_file_path
-            with open(full_file_path, "rb") as file_handler:
-                data = file_handler.read()
-                _put(key, data)
+            md5sum = md5(full_file_path)
+            meta_table[full_file_path] = md5sum
+            if md5sum in files:
+                print(full_file_path + " [Skipped]")
+            else:
+                print(full_file_path)
+                with open(full_file_path, "rb") as file_handler:
+                    data = file_handler.read()
+                    _put_data(md5sum, data)
 
 
 def backup_dir(path):
     _backup_dir(path)
-    _put(META_TABLE_KEY, meta_table.to_json())
+    _put(META_TABLE_KEY, meta_table.to_json().encode())
 
 
 def restore_to(target):
-    for key in _keys():
-        filename = os.path.join(target, key)
+
+    meta_table_data = _get(META_TABLE_KEY)
+    meta_table.from_json(meta_table_data.decode())
+    for partial_filename, azure_key in meta_table.items():
+        filename = os.path.join(target, partial_filename)
         dirname = os.path.dirname(filename)
         if not os.path.exists(dirname):
             os.makedirs(dirname, exist_ok=True)
+        print(filename)
         with open(filename, "wb") as f:
             print(filename)
-            data = _get(key)
+            data = _get_data(azure_key)
             f.write(data)
 
 
@@ -109,10 +139,7 @@ def init(timestamp):
     global _prefix, _nonce, _secret_key, _password
     if _password is None:
         _password = getpass("Cyberpassword: ")
-    if _partition == "":
-        _prefix = timestamp + "/"
-    else:
-        _prefix = _partition + "/" + timestamp + "/"
+    _prefix = _partition + "/" + timestamp + "/"
     _nonce = hashlib.md5(_prefix.encode()).digest()
     _secret_key = hashlib.md5(_password.encode()).digest()
     meta_table_data = _get(META_TABLE_KEY) or "{}"
@@ -120,13 +147,13 @@ def init(timestamp):
 
 
 @click.group()
-@click.option("--partition", default="")
+@click.option("--partition", default="default")
 def main(partition):
     global _partition
     _partition = partition
 
 
-@click.option("--directories", default=False)
+@click.option("--directories")
 @main.command()
 def upload(directories):
     timestamp = datetime.now().strftime("%Y-%m-%d-%H")

c9c6e32268990451a663aae9b342f61fbea8e5ed
Maik Wild <mail@maikwild.de>
2020-07-23 16:55:27 +0200

    implement progress bar

--- azure_snapshotter/snapshotter.py
+++ azure_snapshotter/snapshotter.py
@@ -40,10 +40,27 @@ _prefix = None
 _secret_key = None
 _password = os.environ.get("AZURE_SNAPSHOTTER_CYBERPASSWORD")
 _partition = None
+_progress_bytes = 0
+_total_bytes = 1
 META_TABLE_KEY = "AZURE_SNAPSHOTTER_META_TABLE_KEY"
 TEMP_FOLDER = "/var/tmp/azure_snapshotter/"
 PY_CRYPT_BUFFER_SIZE = 64 * 1024
+_force = False
+_max_line_length = 100
 meta_table = MetaTable()
+
+
+def update_progress(status_str, key = ""):
+    percent = ("{0:.1f}").format(100 * (_progress_bytes / float(_total_bytes)))
+    filledLength = int(41 * _progress_bytes / _total_bytes)
+    bar = "█" * filledLength + '-' * (40 - filledLength)
+    output_buffer = f'\r {human_readable_size(_progress_bytes)} |{bar}| {human_readable_size(_total_bytes)} | {percent}% {status_str}'
+    global _max_line_length
+    if len(output_buffer) > _max_line_length:
+        _max_line_length = len(output_buffer)
+    output_buffer += " " * (_max_line_length - len(output_buffer))
+    print(output_buffer, end = "\r")
+
 
 
 def temp_file_name():
@@ -57,6 +74,20 @@ def rm_rf(file_name):
         else:
             os.remove(file_name)
 
+def dir_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
+def human_readable_size(num, suffix='B'):
+    for unit in ['','k','M','G']:
+        if abs(num) < 1000:
+            return "%3.1f %s%s" % (num, unit, suffix)
+        num /= 1000.0
+    return "%.1f %s%s" % (num, 'T', suffix)
 
 def calculate_nonce(seed_str):
     return hashlib.md5(seed_str.encode()).digest()
@@ -94,11 +125,13 @@ def remove_nonce(file_name, nonce):
 
 
 def _put_file(key, file_name):
+    update_progress("[Encrypting] " +file_name)
     cleartext_file = temp_file_name()
     copyfile(file_name, cleartext_file)
     add_nonce(cleartext_file, calculate_nonce(key))
     cyber_file = temp_file_name()
     pyAesCrypt.encryptFile(cleartext_file, cyber_file, _password, PY_CRYPT_BUFFER_SIZE)
+    update_progress("[Uploading]  " +file_name)
     _store.put_file(_partition + "/files/" + key, cyber_file)
     rm_rf(cleartext_file)
     rm_rf(cyber_file)
@@ -156,15 +189,18 @@ def _backup_dir(path, files=None):
         else:
             md5sum = md5(full_file_path)
             meta_table[full_file_path] = md5sum
-            if md5sum in files:
-                print(full_file_path + " [Skipped]")
+            if md5sum in files and not _force:
+                update_progress("[Skipped]    " + full_file_path)
             else:
-                print(full_file_path)
                 _put_file(md5sum, full_file_path)
+            file_size = os.path.getsize(full_file_path)
+            global _progress_bytes
+            _progress_bytes += file_size
 
 
 def backup_dir(path):
     _backup_dir(path)
+    update_progress("Uploading meta table")
     _put(META_TABLE_KEY, meta_table.to_json().encode())
 
 
@@ -202,15 +238,25 @@ def main(partition):
 
 
 @click.option("--directories")
+@click.option("--force", is_flag=True, default=False)
 @main.command()
-def upload(directories):
+def upload(directories, force):
+    global _total_bytes, _force
+    _force = force
     timestamp = datetime.now().strftime("%Y-%m-%d-%H")
     init(timestamp)
+    directories_lines = None
+
+    update_progress("Calculating upload size...")
     with open(directories, "r") as f:
-        for line in [line.rstrip() for line in f]:
-            print("Transfering " + line)
-            backup_dir(line)
+        directories_lines = [line.rstrip() for line in f]
+    for line in directories_lines:
+        _total_bytes += dir_size(line)
+    for directory_path in directories_lines:
+        backup_dir(directory_path)
     rm_rf(TEMP_FOLDER)
+    print()
+    print("Success")
 
 
 @click.option("--timestamp", required=True)

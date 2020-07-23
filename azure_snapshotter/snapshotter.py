50a8f746a51787cf0bf49c1b62ac4fedeed9eda9
Maik Wild <mail@maikwild.de>
2020-07-23 18:39:49 +0200

    Implement in memeory encryption for small files
    implement memory mode

--- azure_snapshotter/snapshotter.py
+++ azure_snapshotter/snapshotter.py
@@ -22,6 +22,7 @@ params = {
 @dataclass(frozen=True)
 class MetaData:
     md5sum: str
+    encryption_mode: str
 
     def serialize(self):
         return self.__dict__
@@ -50,17 +51,16 @@ _max_line_length = 100
 meta_table = MetaTable()
 
 
-def update_progress(status_str, key = ""):
+def update_progress(status_str, key=""):
     percent = ("{0:.1f}").format(100 * (_progress_bytes / float(_total_bytes)))
     filledLength = int(41 * _progress_bytes / _total_bytes)
-    bar = "█" * filledLength + '-' * (40 - filledLength)
-    output_buffer = f'\r {human_readable_size(_progress_bytes)} |{bar}| {human_readable_size(_total_bytes)} | {percent}% {status_str}'
+    bar = "█" * filledLength + "-" * (40 - filledLength)
+    output_buffer = f"\r {human_readable_size(_progress_bytes)} |{bar}| {human_readable_size(_total_bytes)} | {percent}% {status_str}"
     global _max_line_length
     if len(output_buffer) > _max_line_length:
         _max_line_length = len(output_buffer)
     output_buffer += " " * (_max_line_length - len(output_buffer))
-    print(output_buffer, end = "\r")
-
+    print(output_buffer, end="\r")
 
 
 def temp_file_name():
@@ -74,7 +74,8 @@ def rm_rf(file_name):
         else:
             os.remove(file_name)
 
-def dir_size(start_path = '.'):
+
+def dir_size(start_path="."):
     total_size = 0
     for dirpath, dirnames, filenames in os.walk(start_path):
         for f in filenames:
@@ -82,12 +83,14 @@ def dir_size(start_path = '.'):
             total_size += os.path.getsize(fp)
     return total_size
 
-def human_readable_size(num, suffix='B'):
-    for unit in ['','k','M','G']:
+
+def human_readable_size(num, suffix="B"):
+    for unit in ["", "k", "M", "G"]:
         if abs(num) < 1000:
             return "%3.1f %s%s" % (num, unit, suffix)
         num /= 1000.0
-    return "%.1f %s%s" % (num, 'T', suffix)
+    return "%.1f %s%s" % (num, "T", suffix)
+
 
 def calculate_nonce(seed_str):
     return hashlib.md5(seed_str.encode()).digest()
@@ -124,17 +127,40 @@ def remove_nonce(file_name, nonce):
         f.truncate()
 
 
-def _put_file(key, file_name):
-    update_progress("[Encrypting] " +file_name)
+def _put_file_streaming(file_name, key):
+    update_progress("[Encrypting] " + file_name)
     cleartext_file = temp_file_name()
     copyfile(file_name, cleartext_file)
     add_nonce(cleartext_file, calculate_nonce(key))
     cyber_file = temp_file_name()
-    pyAesCrypt.encryptFile(cleartext_file, cyber_file, _password, PY_CRYPT_BUFFER_SIZE)
-    update_progress("[Uploading]  " +file_name)
+    pyAesCrypt.encryptFile(
+        cleartext_file, cyber_file, _password, PY_CRYPT_BUFFER_SIZE
+    )
+    update_progress("[Uploading]  " + file_name)
     _store.put_file(_partition + "/files/" + key, cyber_file)
     rm_rf(cleartext_file)
     rm_rf(cyber_file)
+
+def _put_file_in_memory(file_name, key):
+    update_progress("[Uploading]  " + file_name)
+    with open(file_name, "rb") as f:
+        _put(key, f.read())
+
+def _put_file(key, file_name, size, skip):
+    if size > 16777216:
+        encryption_mode = "streaming"
+        if skip:
+            update_progress("[Skipped]    " + file_name)
+        else:
+            _put_file_streaming(file_name, key)
+    else:
+        encryption_mode = "in-memory"
+        if skip and False:
+            update_progress("[Skipped]    " + file_name)
+        else:
+            _put_file_in_memory(file_name, key)
+
+    meta_table[file_name] = MetaData(md5sum=key, encryption_mode=encryption_mode)
 
 
 def _get(key):
@@ -145,19 +171,29 @@ def _get(key):
     return decrypt(ciphertext, calculate_nonce(key))
 
 
-def _get_file(key, target):
-    cyber_file = temp_file_name()
-    cleartext_file = temp_file_name()
-    try:
-        with open(cyber_file, "wb") as f:
-            _store.get_file(_partition + "/files/" + key, f)
-    except KeyError:
-        print("Warning: Expected file " + key + " missing.")
-    pyAesCrypt.decryptFile(cyber_file, cleartext_file, _password, PY_CRYPT_BUFFER_SIZE)
-    remove_nonce(cleartext_file, calculate_nonce(key))
-    copyfile(cleartext_file, target)
-    rm_rf(cyber_file)
-    rm_rf(cleartext_file)
+def _get_file(meta_data, target):
+    key = meta_data["md5sum"]
+    encryption_mode = meta_data["encryption_mode"]
+    if encryption_mode == "in-memory":
+        with open(target, "wb") as f:
+            f.write(_get(key))
+    elif encryption_mode == "streaming":
+        cyber_file = temp_file_name()
+        cleartext_file = temp_file_name()
+        try:
+            with open(cyber_file, "wb") as f:
+                _store.get_file(_partition + "/files/" + key, f)
+        except KeyError:
+            print("Warning: Expected file " + key + " missing.")
+        pyAesCrypt.decryptFile(
+            cyber_file, cleartext_file, _password, PY_CRYPT_BUFFER_SIZE
+        )
+        remove_nonce(cleartext_file, calculate_nonce(key))
+        copyfile(cleartext_file, target)
+        rm_rf(cyber_file)
+        rm_rf(cleartext_file)
+    else:
+        raise ValueError(f"Invalid memory mode: {encryption_mode}")
 
 
 def _keys():
@@ -188,12 +224,11 @@ def _backup_dir(path, files=None):
             _backup_dir(full_file_path, files)
         else:
             md5sum = md5(full_file_path)
-            meta_table[full_file_path] = md5sum
-            if md5sum in files and not _force:
-                update_progress("[Skipped]    " + full_file_path)
-            else:
-                _put_file(md5sum, full_file_path)
             file_size = os.path.getsize(full_file_path)
+            skip = False
+            if md5sum in files and not _force:
+                skip = True
+            _put_file(md5sum, full_file_path, file_size, skip)
             global _progress_bytes
             _progress_bytes += file_size
 
@@ -205,16 +240,15 @@ def backup_dir(path):
 
 
 def restore_to(target):
-
     meta_table_data = _get(META_TABLE_KEY)
     meta_table.from_json(meta_table_data.decode())
-    for partial_filename, azure_key in meta_table.items():
+    for partial_filename, meta_data in meta_table.items():
         filename = os.path.join(target, partial_filename)
         dirname = os.path.dirname(filename)
         if not os.path.exists(dirname):
             os.makedirs(dirname, exist_ok=True)
         print(filename)
-        _get_file(azure_key, filename)
+        _get_file(meta_data, filename)
 
 
 def init(timestamp):
